b4756447f024ddd47c1d035a95ebcdfde1543dd6
Anand Kumar Mainali <urajmainali5@gmail.com>
2019-01-02 14:49:18 +0545

    Created Burger Design

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Aux from "../../hoc/Aux";
-import classes from "./Layout.css";
+import Aux from "../../hoc/Auxiliary";
+import classes from "./Layout.module.css";
 const layout = props => (
   <Aux>
     <div>Toolbar, Sidedrawer, Backdrop</div>
